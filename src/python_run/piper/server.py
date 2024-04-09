4151b713a435096c3d837ffcd684929969441cd5
Aus Gomez <austrebertog@gmail.com>
2024-04-09 13:13:19 -0500

    Update server.py

--- src/python_run/piper/server.py
+++ src/python_run/piper/server.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-from download import getVoices
-from voice import PiperVoice
+from .download import getVoices
+from . import PiperVoice
 
 @app.route('/server')
 def server():
